8752e3408a6eb0a7965e37cf02969a92a1725e34
navin-mawa <navin.mawa.navin.mawa@gmail.com>
2021-08-13 09:53:15 +0100

    Pushed up spring Cars

---
+++ backend/spring-car/src/test/java/com/qa/springcar/SpringCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.springcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
